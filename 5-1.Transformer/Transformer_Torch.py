6361dcb4810826a51be73595780cc8ea5bbaaf36
Jiangjinrui1 <202130462448@mail.scut.edu.cn>
2024-03-13 17:13:19 +0800

    1

--- 5-1.Transformer/Transformer_Torch.py
+++ 5-1.Transformer/Transformer_Torch.py
@@ -336,11 +336,11 @@ def greedy_decoder(model, enc_input, start_symbol):
     return dec_input
 
 # Test
-enc_inputs, _, _ = next(iter(loader))
-enc_inputs = enc_inputs.cuda()
-for i in range(len(enc_inputs)):
-    greedy_dec_input = greedy_decoder(model, enc_inputs[i].view(1, -1), start_symbol=tgt_vocab["S"])
-    predict, _, _, _ = model(enc_inputs[i].view(1, -1), greedy_dec_input)
-    predict = predict.data.max(1, keepdim=True)[1]
-    print(enc_inputs[i], '->', [idx2word[n.item()] for n in predict.squeeze()])
-
+if __name__ == '__main__':
+    enc_inputs, _, _ = next(iter(loader))
+    enc_inputs = enc_inputs.cuda()
+    for i in range(len(enc_inputs)):
+        greedy_dec_input = greedy_decoder(model, enc_inputs[i].view(1, -1), start_symbol=tgt_vocab["S"])
+        predict, _, _, _ = model(enc_inputs[i].view(1, -1), greedy_dec_input)
+        predict = predict.data.max(1, keepdim=True)[1]
+        print(enc_inputs[i], '->', [idx2word[n.item()] for n in predict.squeeze()])
